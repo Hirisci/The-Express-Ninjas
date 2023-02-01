84f0aa9d22f5521a997e10c7573fde038a4977eb
Hirisci <www.alan92@icloud.com>
2023-02-01 14:33:25 +0100

    Aggiungere i file di progetto.

---
+++ 3layer/Program.cs
@@ -0,0 +1,32 @@
+using DAL.Database;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// add services to the DB
+builder.Services.AddDbContext<AppDbContext>(
+        options => options.UseSqlServer("Data Source=localhost;Initial Catalog=db-FucinaEroi;Integrated Security=True;Pooling=False"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ DAL/Database/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=db-FucinaEroi;Integrated Security=True;Pooling=False");
+        }
+    }
+}
